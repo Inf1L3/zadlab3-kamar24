339ac7e01e9ba41c98fa8443daa59d8d51f0654c
Lukasz <Lukasz@Komp>
2018-03-15 19:44:18 +0100

    Zadania

--- POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Main.java
@@ -5,7 +5,7 @@
  */
 package pl.edu.ur.POLab3;
 
-import java.util.Scanner;
+
 
 /**
  *
@@ -13,94 +13,11 @@ import java.util.Scanner;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO 1.4, 1.7, 
-        // TODO 2.2
-        // TODO 3.3, 3.4
-    /*int a = 6; // 110
-    int b = 4; // 100
-
-    // Bitwise AND    
-
-    int c = a & b;
-    //   110
-    // & 100
-    // -----
-    //   100
-
-    // Bitwise OR
-
-    int d = a | b;
-    //   110
-    // | 100
-    // -----
-    //   110
-
-System.out.println(c); // 4
-System.out.println(d); // 6 
-    
-    int a=2;
-    int b=5;
-     System.out.println("LUB");
-    if(a%2==0 || b%2==0){
-        System.out.println("Prawda");}
-        else{
-        System.out.println("Falsz");}
-    System.out.println("I");
-        
-    if(a%2==0 && b%2==0){
-        System.out.println("Prawda");}
-        else{
-        System.out.println("Falsz");}
-       
-     {
-     String Str1 = new String("This is really not immutable!!");
-      String Str2 = Str1;
-      String Str3 = new String("This is really not immutable!!");
-      String Str4 = new String("This IS REALLY NOT IMMUTABLE!!");
-      boolean retVal;
-
-      retVal = Str1.equals( Str2 );
-      System.out.println("Returned Value = " + retVal );
-
-      retVal = Str1.equals( Str3 );
-      System.out.println("Returned Value = " + retVal );
-
-      retVal = Str1.equalsIgnoreCase( Str4 );
-      System.out.println("Returned Value = " + retVal );
-   }
-    */
-    int s=1;
-    int a=5;
-    //Scanner sc= new Scanner(System.in);
-    //a= sc.nextInt();
-     int i;
-    for(i=a;i>0;i--){
-        s=s*(a-1);}
-    System.out.println("Silnia z "+a+"wynosi:" +s);    
-    }}
-    
-    /*Scanner sc = new Scanner(System.in);
-    System.out.print("Podaj liczbę: " ); 
-    // pobieramy od użytkownika liczbę
-    int liczba = sc.nextInt(); 
-    // wyświetlamy na ekranie obliczoną silnię
-    System.out.println(liczba + "! = " + silnia(liczba));
- }
-    
-    
- public static int silnia(int wartosc) {
-// jeśli przekazany parametr jest równy zero to zwróć 1
-// a w przeciwnym wypadku zwróć wartość parametru * wywołanie metody silnia
-// z parametrem o jeden mniejszym
-if(wartosc == 0) return 1;
-else return wartosc * silnia(wartosc - 1);   
- } */
-    
-    
+    Zadania.menu();
+    }} 
+   
  
     
     
